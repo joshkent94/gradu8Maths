c6e5b5f4aac2d0520cb55d4368525eec30aa032a
joshkent94 <josh.kent94@yahoo.co.uk>
2021-11-08 22:13:50 +0000

    Ficing FAQ section

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -7,18 +7,18 @@ export default function Faq(props) {
         const button = e.target;
         button.classList.toggle('active-faq');
         const answer = document.getElementById(faq.answer);
-        answer.classList.toggle('hidden');
+        answer.classList.toggle('show');
     };
 
     return (
         <div className="card faqCard wow fadeIn" data-wow-delay="0.4s">
-                <button className="card-header btn faqBtn" type="button" name={faq.question} onClick={handleClick}>
-                    <FontAwesomeIcon icon='angle-right' className='dropdown-arrow' />
-                    {faq.question}
-                </button>
-            <div id={faq.answer} className="card-body hidden">
-                    {faq.answer}
-                </div>
+            <button className="card-header btn faqBtn" type="button" name={faq.question} onClick={handleClick}>
+                <FontAwesomeIcon icon='angle-right' className='dropdown-arrow' />
+                {faq.question}
+            </button>
+            <div id={faq.answer} className="card-body">
+                {faq.answer}
+            </div>
         </div>
     );
 };
