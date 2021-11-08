1992bf718e9e288b5e09a419ac34e50b6cc46aef
joshkent94 <josh.kent94@yahoo.co.uk>
2021-11-08 20:13:02 +0000

    Fixing FAQ toggles

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -3,13 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Faq(props) {
     const faq = props.faq;
 
+    const handleClick = e => {
+        const button = e.target;
+        button.classList.toggle('active-faq');
+        const answer = document.getElementById(faq.answer);
+        answer.classList.toggle('hidden');
+    };
+
     return (
         <div className="card faqCard wow fadeIn" data-wow-delay="0.4s">
-                <button className="card-header btn faqBtn" type="button">
+                <button className="card-header btn faqBtn" type="button" name={faq.question} onClick={handleClick}>
                     <FontAwesomeIcon icon='angle-right' className='dropdown-arrow' />
                     {faq.question}
                 </button>
-                <div className="card-body">
+            <div id={faq.answer} className="card-body hidden">
                     {faq.answer}
                 </div>
         </div>
