03c24068371ced4a9e6b607378f9547ebf8c72b4
joshkent94 <josh.kent94@yahoo.co.uk>
2021-11-18 23:37:38 +0000

    Fixing a few CSS bugs

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -10,8 +10,9 @@ export default function Heading() {
                     <div className="container">
                         <div className="row g-0">
                             <div className="col-md-12 mb-4 text-center wow fadeInUp centerTitle">
-                                <h1 className=" h1-reponsive text-uppercase font-weight-bold wow fadeInDown" data-wow-delay="0.3s"><strong>Gradu8 Maths</strong><hr className="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s" /></h1>
-                                <h5 className="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Mathematics Tuition in Doncaster</strong></h5>
+                                <h1 className="main-title remove-margin text-uppercase wow fadeInDown" data-wow-delay="0.3s"><strong>Gradu8 Maths</strong></h1>
+                                <hr className="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s" />
+                                <h5 className="sub-title text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Mathematics Tuition in Doncaster</strong></h5>
                             </div>
                         </div>
                     </div>
