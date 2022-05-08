391debb584027247d307b6928a335c33705fe886
joshkent94 <josh.kent94@yahoo.co.uk>
2022-05-08 20:29:19 +0100

    Changes to faq section

--- src/components/Faqs/Faqs.js
+++ src/components/Faqs/Faqs.js
@@ -11,8 +11,8 @@ export default function Faqs() {
                 <h2 className="text-center wow fadeInDown" data-wow-delay="0.4s">Frequently Asked Questions (FAQs)</h2>
             </div>
 
-            <p className="text-center remove-margin wow fadeInDown" data-wow-delay="0.4s">Many students and parents have asked me what is required to be accepted into top universities such as Oxford and Cambridge. In the video below I sit down with my former pupil, Ilya, who was accepted into Cambridge, to discuss his journey and ask for his tips on how others can follow suit.</p>
-            <EmbeddedVideo src={"https://www.youtube.com/embed/onOqVPeFT94"} />
+            <p className="text-center remove-margin wow fadeInDown" data-wow-delay="0.4s">Students and parents often ask me what is required to secure a place at Oxford or Cambridge. In this video I sit down with my former pupil Illya, who is presently studying at Cambridge, to discuss his journey and ask for tips so that others may follow in his footsteps.</p>
+            <EmbeddedVideo src={"https://www.youtube.com/embed/WSuWoX0tkU0"} />
 
             <Accordion>
                 {faqList.map(faq => {
